bb33066bd5ee3d4a74a53391ab8abf4f57ab5c57
LuanGBR <luangustavodebrito@gmail.com>
2022-11-10 00:34:01 -0300

    many changes

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
-from turtle import width
-import mujoco_py
+#Train robot to catch ball on mujoco with RL
+
+import mujoco_py as mjp
+import random
 import os
-from PIL import Image
-mj_path = mujoco_py.utils.discover_mujoco()
-xml_path = "/home/larissa/Documents/TCC/simulacao/content/environment.xml"
-print(xml_path)
-model = mujoco_py.load_model_from_path(xml_path)
-sim = mujoco_py.MjSim(model)
 
-
-print([(x.geom1,x.geom2) for x in sim.data.contact])
-
-
-# win = mujoco_py.MjViewer(sim)
-
-
-# [0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+def main():
+    # Load model
+    model = mjp.load_model_from_path("model.xml")
+    sims = [mjp.MjSim(model)]
 
 
 
-sim.step() 
-cam_img = sim.render(width=800,height=600,camera_name="depth_camera1")
-Image.fromarray(cam_img[::-1]).show()
-print(sim.data.qpos)
 
-input()
+    
+
+if __name__ == "__main__": 
+    main()
